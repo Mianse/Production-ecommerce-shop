1f743586042f6dd2a1f8b58eaa75f2ab37837e4a
damian orina <96182161+Mianse@users.noreply.github.com>
2024-02-16 01:53:06 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import morgan from 'morgan';
 import connectDB from './config/db.js'
 import authRoute from './routes/authRoute.js'
 import cors from 'cors'
-import CategoryRoute from './routes/categoryRoute.js'
+import categoryRoute from './routes/categoryRoute.js'
 import productRoute from './routes/productRoute.js'
 import path from 'path'
 import { fileURLToPath } from 'url';
@@ -30,7 +30,7 @@ app.use(express.static(path.join(__dirname,'./client/build')))
 
 //routes
 app.use('/api/v1/auth',authRoute)
-app.use('/api/v1/category', CategoryRoute)
+app.use('/api/v1/category', categoryRoute)
 app.use('/api/v1/product',productRoute)
 
 //rest api
