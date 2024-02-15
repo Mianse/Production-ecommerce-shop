262f4a94f66bc3b69c29826dbc7b68ae1eae5530
damian orina <96182161+Mianse@users.noreply.github.com>
2024-02-16 00:35:23 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ import authRoute from './routes/authRoute.js'
 import cors from 'cors'
 import CategoryRoute from './routes/categoryRoute.js'
 import productRoute from './routes/productRoute.js'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
 //cnfigure env
 dotenv.config()
 
@@ -19,12 +22,16 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname,'./client/build')))
+
 //routes
 app.use('/api/v1/auth',authRoute)
 app.use('/api/v1/category', CategoryRoute)
 app.use('/api/v1/product',productRoute)
 
 //rest api
-
+app.use('*',function(req,res){
+    res.sendFile(path.join(__dirname,'./client/build/index.html'))
+})
 const PORT = process.env.pORT || 5000
 app.listen(PORT,()=> console.log(`server running on ${process.env.DEV_MODE} on port ${PORT}`.bgCyan.white))
