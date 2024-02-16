7684eafea9830adf5008bc8c5b5405eda6d802f8
damian orina <96182161+Mianse@users.noreply.github.com>
2024-02-16 12:16:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ dotenv.config()
 connectDB();
 
 //esmodulefix
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename);
 
 //rest object
 const app = express()
